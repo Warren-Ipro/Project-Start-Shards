4f86a18f7dd99c9c6fb64220edf7a6ecf4335f64
Warren-Ipro <warren@iprogroup.ca>
2019-06-26 15:16:45 -0700

    Added PRroject start.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  // Back To Top Button
+  $('.container').on('click', '.back-to-top', function (e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop: 0}, 800);
+  });
+
+  $('.hello').on('click', (e) => {
+    e.preventDefault()
+    $('h3, p').toggle('slow')
+  })
+})
+
+$('#datepicker-example').datepicker({});
